1bef8bf4d8d17e7d1cb1ccf9667fba98298acbf3
serhatyildiz <yildiz_serhat@hotmail.com>
2023-04-05 21:27:35 +0200

    Initial commit

---
+++ src/main/java/com/yildiz/serhat/favouriterecipesservice/FavouriteRecipesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yildiz.serhat.favouriterecipesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavouriteRecipesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FavouriteRecipesServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yildiz/serhat/favouriterecipesservice/FavouriteRecipesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yildiz.serhat.favouriterecipesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavouriteRecipesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
